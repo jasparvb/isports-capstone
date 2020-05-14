797a2bec53c844d736203f5af4dc3fed126cbaba
von Buelow <jaspar@UMICH.EDU>
2020-05-14 09:33:31 -0400

    iSports models

---
+++ models.py
@@ -0,0 +1,129 @@
+"""Models for iSports app."""
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+
+# MODELS
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    username = db.Column(db.String(50), unique=True, nullable=False)
+
+    password = db.Column(db.Text, nullable=False)
+
+    email = db.Column(db.String(50), nullable=True)
+
+    favorites = db.relationship( 'Favorite', backref="user", cascade="all, delete")
+
+    languages = db.relationship( 'Language', secondary='user_language', backref='users')
+
+    follows = db.relationship( 'Follow', backref="users")
+  
+    def __repr__(self):
+        u = self
+        return f"<User username={u.username} email={u.email}>"
+
+    @classmethod
+    def register (cls, username, pwd, email):
+        """Register user with hashed password and return user"""
+
+        hashed = bcrypt.generate_password_hash(pwd)
+        hashed_utf8 = hashed.decode("utf8")
+        user = cls(username=username, password=hashed_utf8, email=email)
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def login (cls, username, pwd):
+        """Check to see if user exists and return user if valid, else return False"""
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, pwd):
+            return user
+        else:
+            return False
+
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    title = db.Column(db.Text, nullable=False)
+
+    source = db.Column(db.String(255), nullable=False)
+
+    url = db.Column(db.Text, nullable=False)
+
+    image_url = db.Column(db.Text, nullable=False)
+
+    published_at = db.Column(db.String(50), nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+ 
+    def __repr__(self):
+        u = self
+        return f"<Favorite title={u.title}>"
+
+
+class Follow(db.Model):
+    __tablename__ = 'follows'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    name = db.Column(db.Text, nullable=False)
+
+    sportsdb_id = db.Column(db.Integer, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False, ondelete='cascade')
+
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False, ondelete='cascade')
+
+    category = db.relationship( 'Category', backref="follows")
+ 
+    def __repr__(self):
+        u = self
+        return f"<Follow name={u.name}>"
+
+
+class UserLanguage(db.Model):
+    __tablename__ = 'user_language'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False, ondelete='cascade')
+
+    language_id = db.Column(db.Integer, db.ForeignKey('languages.id'), nullable=False, ondelete='cascade')
+
+ 
+class Language(db.Model):
+    __tablename__ = 'languages'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    name = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        u = self
+        return f"<Language name={u.name}>"
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    name = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        u = self
+        return f"<Category name={u.name}>"
