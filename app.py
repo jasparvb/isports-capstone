29500d966e482f7fe5c23329ca5ac30b8311c680
von Buelow <jaspar@UMICH.EDU>
2020-05-15 19:23:59 -0400

    User register, login, logout routes

---
+++ app.py
@@ -0,0 +1,119 @@
+"""Flask app for iSports"""
+
+from flask import Flask, request, redirect, render_template, flash, jsonify, session, g
+from models import db, connect_db, User, Favorite, Follow
+from forms import AddUserForm, LoginUserForm, AddFollow
+from isports import get_top_news
+
+CURR_USER_KEY = "curr_user"
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:41361@localhost/isports"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+from flask_debugtoolbar import DebugToolbarExtension
+app.config['SECRET_KEY'] = "SECRET!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def home():
+    """Render homepage with top sports news headlines"""
+    articles = get_top_news()
+    return render_template("home.html", articles=articles)
+
+############################################################################################
+# Signup/Login/Logout
+############################################################################################
+
+@app.before_request
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    """Handle user signup.
+
+    Create new user and add to DB. Redirect to home page.
+
+    If form not valid, present form.
+
+    If the there already is a user with that username: flash message
+    and re-present form.
+    """
+
+    form = AddUserForm()
+
+    if form.validate_on_submit():
+        try:
+            user = User.signup(
+                username=form.username.data,
+                password=form.password.data,
+                email=form.email.data,
+            )
+            db.session.commit()
+
+        except IntegrityError:
+            flash("Username already taken", 'danger')
+            return render_template('signup.html', form=form)
+
+        do_login(user)
+
+        return redirect("/")
+
+    else:
+        return render_template('signup.html', form=form)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Handle user login."""
+
+    form = LoginUserForm()
+
+    if form.validate_on_submit():
+        user = User.login(form.username.data,
+                                 form.password.data)
+
+        if user:
+            do_login(user)
+            flash(f"Hello, {user.username}!", "success")
+            return redirect("/")
+
+        flash("Invalid credentials.", 'danger')
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """Handle logout of user."""
+
+    do_logout()
+    flash("You have logged out", 'success')
+    return redirect("/login")
+
