80f3097aac530ae40f0ec1b9e2d91c0a5627c24a
von Buelow <jaspar@UMICH.EDU>
2020-05-16 23:49:35 -0400

    Autocomplete complete, modified result string

--- static/main.js
+++ static/main.js
@@ -1,4 +1,8 @@
-$(function(){
+$(async function(){
+
+    const sports = await getSports();
+    const leagues = await getLeagues();
+    
 
     async function getSports() {
         const res = await axios.get("https://www.thesportsdb.com/api/v1/json/1/all_sports.php");
@@ -14,7 +18,7 @@ $(function(){
     async function getLeagues() {
         const res = await axios.get("https://www.thesportsdb.com/api/v1/json/1/all_leagues.php");
         return res.data.leagues.map(val => {
-            return {label: val.strLeague,
+            return {label: `${val.strLeague} (${val.strSport})`,
                     value: val.strLeague,
                     category: "league",
                     dbid: val.idLeague,
@@ -26,7 +30,7 @@ $(function(){
         const res = await axios.get(`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${term}`);
         if(res.data.teams){
             return teams = res.data.teams.map( val => {
-                return {label: val.strTeam,
+                return {label: `${val.strTeam} - ${val.strLeague} (${val.strCountry})`,
                         value: val.strTeam,
                         category: "team",
                         dbid: val.idTeam,
@@ -39,8 +43,8 @@ $(function(){
     async function getPlayers(term) {
         const res = await axios.get(`https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=${term}`)
         if(res.data.player){
-            players = res.data.player.map( val => {
-                return {label: val.strPlayer,
+            return players = res.data.player.map( val => {
+                return {label: `${val.strPlayer} (${val.strTeam} - ${val.strPosition})`,
                     value: val.strPlayer,
                     category: "player",
                     dbid: val.idPlayer,
@@ -55,17 +59,15 @@ $(function(){
         source: async function (request, response) {
             let teams = await getTeams(request.term);
             let players = await getPlayers(request.term);
-            let sports = await getSports();
-            let leagues = await getLeagues();
-            console.log(teams);
-            let data = sports.concat(players, teams, leagues);
-            response(data);
+            
+            let data = players.concat(teams, sports, leagues);
+            response($.ui.autocomplete.filter(data, request.term));
         },
         // Once a value in the drop down list is selected, do the following:
         select: function(event, ui) {
             // place extra attributes into hidden text fields
             $('#follow-form #thumb').val(ui.item.thumb);
-            $('#follow-form #dbid').val(ui.item.dbid);
+            $('#follow-form #sportsdb_id').val(ui.item.dbid);
             $('#follow-form #category').val(ui.item.category);
             return false;
         }
